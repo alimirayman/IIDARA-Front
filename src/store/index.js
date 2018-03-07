31b2a7c9d1dcda10a98bc87cc5d42c5d686c7b4c
Mir Ayman Ali <alimirayman@outlook.com>
2018-03-07 09:01:25 +0600

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {}
+const getters = {}
+const mutations = {}
+const actions = {}
+const modules = {}
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+  modules
+})
+
+export default store
