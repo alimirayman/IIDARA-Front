87dc93e41b8559cb6cbd0eb2c5a7abdb0d7c27f0
Mir Ayman Ali <alimirayman@outlook.com>
2018-03-09 14:18:37 +0600

    add karigor cards to dashboard

--- src/Pages/DashBoard.vue
+++ src/Pages/DashBoard.vue
@@ -1,17 +1,36 @@
 <template>
   <div>
-    <addCard></addCard>
+    <!-- Add Card -->
+    <section>
+      <addCard></addCard>
+    </section>
+    <section>
+      <div class="display-3 text-center mb-4">Karigor</div>
+      <div class="row">
+        <div class="col-md-6 col-lg-4" v-for="k in karigor" :key="k.id">
+          <userCardGroup :id="k.id"></userCardGroup>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import addCard from '@/components/AddCard'
+import userCardGroup from '@/components/UserCards'
+import {mapGetters} from 'vuex'
 export default {
+  computed: {
+    ...mapGetters({
+      karigor: 'karigor'
+    })
+  },
   components: {
-    addCard
+    addCard,
+    userCardGroup
   },
   created () {
-    this.$store.dispatch('GET_CARDS')
+    this.$store.dispatch('GET_CARDS', 1)
   }
 }
 </script>
---
+++ src/components/UserCards.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="card shadow-small bg-light border-0 mb-5">
+      <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item shadow-inset" v-for="card in cards" :key="card.id">
+          {{ card.title }} ({{ card.allocated_time }})
+        </li>
+      </ul>
+      <div class="card-body row">
+        <div class="col-sm-4 text-center">
+          <img :src="karigor.avatar" class="img-fluid" :alt="karigor.name">
+        </div>
+        <div class="col-sm-8">
+          <h5 class="card-title">{{ karigor.name }}</h5>
+          <p class="card-text">{{ karigor.role }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import {mapGetters} from 'vuex'
+export default {
+  props: ['id'],
+  computed: {
+    cards () {
+      return this.$store.getters.userCards(this.id)
+    },
+    karigor () {
+      return this.$store.getters.karigorInd(this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+img{
+  border-radius: 50% !important;
+  max-height: 50px;
+}
+</style>
