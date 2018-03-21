46788ef0d742f231ac99af4ccde02763f0348374
SL0TR <mohaiminislam94@gmail.com>
2018-03-22 05:56:51 +0600

    set route and initial layout

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form class="mt-4">
+      <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input v-model="form.email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input v-model="form.password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <div>{{ form.email }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  method: {
+    submit () {
+      this.$store.dispatch('LOGIN', this.form)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,8 @@
 const routes = [
   {path: '/', name: 'DashBoard', component: loadPage('DashBoard')},
   {path: '/s/addUser', name: 'AddUser', component: loadPage('AddUser')},
-  {path: '/p/:username', name: 'Profile', props: true, component: loadPage('Profile')}
+  {path: '/p/:username', name: 'Profile', props: true, component: loadPage('Profile')},
+  {path: '/login', name: 'LoginPage', component: loadPage('Auth/Login')}
 ]
 
 export default routes
